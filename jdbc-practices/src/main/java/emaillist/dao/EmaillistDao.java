e942160f71a3216d02d8bf21e2f6ef8b780bba44
heejinssss <heejin.dev33@gmail.com>
2024-05-17 17:22:10 +0900

    Feat: JDBC-EmailListDaoTest

--- jdbc-practices/src/main/java/emaillist/dao/EmaillistDao.java
+++ jdbc-practices/src/main/java/emaillist/dao/EmaillistDao.java
@@ -3,7 +3,10 @@ package emaillist.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import emaillist.vo.EmaillistVo;
 
@@ -67,7 +70,7 @@ public class EmaillistDao {
 			Class.forName("org.mariadb.jdbc.Driver");
 
 			// 2. 연결
-			String url = "jdbc:mariadb://192.168.0.208:3306/webdb?charset=utf8"; // 연결 url 필수 정의 (jdbc:db://IP:PORT/SCHEMA)
+			String url = "jdbc:mariadb://192.168.0.208:3306/webdb?charset=utf8";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 
 			// 3. Statement 준비하기
@@ -89,6 +92,70 @@ public class EmaillistDao {
 			System.out.println("error: " + e);
 		} finally {
 			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+
+	public List<EmaillistVo> findAll() {
+		List<EmaillistVo> result = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			// 1. JDBC Driver 로딩
+			Class.forName("org.mariadb.jdbc.Driver");
+
+			// 2. 연결
+			String url = "jdbc:mariadb://192.168.0.208:3306/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. Statement 준비하기
+			String sql = "select no, first_name, last_name, email" + " from emaillist" + " order by no desc";
+			pstmt = conn.prepareStatement(sql);
+
+			// 4. parameter("?") binding
+//			pstmt.setString(1, "%" + keyword + "%");
+//			pstmt.setString(2, "%" + keyword + "%");
+
+			// 5. SQL 실행
+			rs = pstmt.executeQuery();
+
+			// 6. 결과 처리
+			while (rs.next()) {
+				Long no = rs.getLong(1);
+				String firstName = rs.getString(2);
+				String lastName = rs.getString(3);
+				String email = rs.getString(4);
+
+				EmaillistVo vo = new EmaillistVo();
+				vo.setNo(no);
+				vo.setFirstName(firstName);
+				vo.setLastName(lastName);
+				vo.setEmail(email);
+
+				result.add(vo);
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패: " + e);
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
 				if (pstmt != null) {
 					pstmt.close();
 				}
--- jdbc-practices/src/test/java/emaillist/dao/EmailListDaoTest.java
+++ jdbc-practices/src/test/java/emaillist/dao/EmailListDaoTest.java
@@ -1,16 +1,29 @@
 package emaillist.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.MethodOrderer;
 
 import emaillist.vo.EmaillistVo;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EmailListDaoTest {
+
+	private static int count = 0;
+
+	@BeforeAll
+	public static void setUp() {
+		List<EmaillistVo> list = new EmaillistDao().findAll();
+		count = list.size(); // 기존 테이블 row 개수 저장
+	}
 
 	@Test
 	@Order(1)
@@ -26,8 +39,20 @@ public class EmailListDaoTest {
 
 	@Test
 	@Order(2)
+	public void testFindAll() {
+		List<EmaillistVo> list = new EmaillistDao().findAll();
+		assertEquals(count + 1, list.size()); // 인스턴스 추가 성공 여부 확인
+	}
+
+	@Test
+	@Order(3)
 	public void testDeleteByEmail() {
 		boolean result = new EmaillistDao().deleteByEmail("naruto@gmail.com");
 		assertTrue(result);
+	}
+
+	@AfterAll
+	public static void cleanup() {
+		// 자원 정리
 	}
 }
