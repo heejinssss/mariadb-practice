5b7b5e97aaa485779268d62668e15642c387f93c
heejinssss <heejin.dev33@gmail.com>
2024-05-20 19:56:20 +0900

    Feat: JDBC-BookVo, BookDao

---
+++ jdbc-practices/src/main/java/bookmall/vo/BookVo.java
@@ -0,0 +1,55 @@
+package bookmall.vo;
+
+public class BookVo {
+	private Long no;
+	private String title;
+	private Integer price;
+	private Long categoryNo;
+	private String categoryName;
+
+	public BookVo(String title, Integer price) {
+		super();
+		this.title = title;
+		this.price = price;
+	}
+
+	public Long getNo() {
+		return no;
+	}
+
+	public void setNo(Long no) {
+		this.no = no;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Long getCategoryNo() {
+		return categoryNo;
+	}
+
+	public void setCategoryNo(Long categoryNo) {
+		this.categoryNo = categoryNo;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+}
