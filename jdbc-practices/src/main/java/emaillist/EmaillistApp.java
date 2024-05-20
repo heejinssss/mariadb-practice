0900f45fa0656e0c8dc73c47625abbdad5e0895d
heejinssss <heejin.dev33@gmail.com>
2024-05-20 10:19:26 +0900

    Refactor: JDBC-EmaillistApp

--- jdbc-practices/src/main/java/emaillist/EmaillistApp.java
+++ jdbc-practices/src/main/java/emaillist/EmaillistApp.java
@@ -9,11 +9,11 @@ import emaillist.vo.EmaillistVo;
 public class EmaillistApp {
 	private static EmaillistVo vo = new EmaillistVo();
 	private static EmaillistDao dao = new EmaillistDao();
+	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		enterMessage();
-		Scanner scanner = new Scanner(System.in);
-		
+
 		while (true) {
 			selectMessage();
 			String command = scanner.nextLine();
@@ -22,37 +22,45 @@ public class EmaillistApp {
 				doList();
 			} else if ("2".equals(command)) { // Delete
 				/* 이메일 존재 여부 검토 코드 필요 */
-				log("삭제하실 이메일을 입력하세요: ");
-				String email = scanner.nextLine();
-				doDelete(email);
+				doDelete();
 			} else if ("3".equals(command)) { // Create
 				/* 이메일 형식 검토 코드 필요 */
-				log("성 >> ");
-				String firstName = scanner.nextLine();
-				log("이름 >> ");
-				String lastName = scanner.nextLine();
-				log("이메일 >> ");
-				String email = scanner.nextLine();
-				doInsert(firstName, lastName, email);
+				doInsert();
 			} else if ("4".equals(command)) {
 				goodbyeMessage();
 				break;
 			}
 		}
 
-		scanner.close();
+		if (scanner != null) {
+			scanner.close();
+		}
 	}
 
-	private static void doInsert(String firstName, String lastName, String email) {
+	private static void doInsert() {
+		log("성 >> ");
+		String firstName = scanner.nextLine();
+		log("이름 >> ");
+		String lastName = scanner.nextLine();
+		log("이메일 >> ");
+		String email = scanner.nextLine();
+
+		EmaillistVo vo = new EmaillistVo();
 		vo.setFirstName(firstName);
 		vo.setLastName(lastName);
 		vo.setEmail(email);
+
 		dao.insert(vo);
-		welcomeMessage(vo.getLastName());
+		doList();
+//		welcomeMessage(vo.getLastName());
 	}
 
-	private static void doDelete(String email) {
+	private static void doDelete() {
+		log("삭제하실 이메일을 입력하세요: ");
+		String email = scanner.nextLine();
+
 		dao.deleteByEmail(email);
+		doList();
 		System.out.println("⋆ ˚｡⋆୨ ʚ " + vo.getLastName() + " ɞ ୧⋆ ˚｡⋆" + "님의 회원 정보가 삭제되었습니다.");
 		comebackMessage();
 	}
@@ -60,10 +68,11 @@ public class EmaillistApp {
 	private static void doList() {
 		List<EmaillistVo> result = dao.findAll();
 		System.out.println();
-        System.out.println("▐░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▌");
-        for (EmaillistVo vo : result) {
-            System.out.println(" [" + vo.getNo() + "] " + vo.getFirstName() + " " + vo.getLastName() + " (" + vo.getEmail() + ")");
-            System.out.println("▐░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▌");
+		System.out.println("▐░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▌");
+		for (EmaillistVo vo : result) {
+			System.out.println(
+					" [" + vo.getNo() + "] " + vo.getFirstName() + " " + vo.getLastName() + " (" + vo.getEmail() + ")");
+			System.out.println("▐░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▌");
 		}
 	}
 
@@ -79,49 +88,27 @@ public class EmaillistApp {
 	}
 
 	private static void enterMessage() {
-	    System.out.println(". ˚\r\n"
-	    		+ "*  * 　　　 ⊹  ˚  .    　　.\r\n"
-	    		+ "⊹  ·  ✧ 　　　*    *\r\n"
-	    		+ ". 　　  ⊹  ✦  　 ✵  　　　　　*\r\n"
-	    		+ "* .　  ·"
-		);
+		System.out.println(". ˚\r\n" + "*  * 　　　 ⊹  ˚  .    　　.\r\n" + "⊹  ·  ✧ 　　　*    *\r\n"
+				+ ". 　　  ⊹  ✦  　 ✵  　　　　　*\r\n" + "* .　  ·");
 	}
 
-	private static void welcomeMessage(String name) {
-		System.out.println();
-		System.out.println("。　♡ 。　　♡。　　♡\r\n"
-				+ "♡。　＼　　｜　　／。　♡\r\n"
-				+ "　 "+name+"님! 환영합니다!\r\n"
-				+ "♡。　／　　｜　　＼。　♡\r\n"
-				+ "。　♡。 　　。　　♡"
-				+ "");
-	}
-	
+//	private static void welcomeMessage(String name) {
+//		System.out.println();
+//		System.out.println("。　♡ 。　　♡。　　♡\r\n" + "♡。　＼　　｜　　／。　♡\r\n" + "　 " + name + "님! 환영합니다!\r\n"
+//				+ "♡。　／　　｜　　＼。　♡\r\n" + "。　♡。 　　。　　♡" + "");
+//	}
+
 	private static void comebackMessage() {
 		System.out.println(".");
 		System.out.println(".");
 		System.out.println(".");
-		System.out.println("　　　가\r\n"
-				+ "　　 아\r\n"
-				+ "　　　지\r\n"
-				+ "　　　　마\r\n"
-				+ "　　　　 ㅇ\r\n"
-				+ "　　　　ㅏ\r\n"
-				+ "　 ヽ＼　 /／\r\n"
-				+ "　　　 ,､,､　ﾟ｡\r\n"
-				+ "　 ﾟ　(ﾟﾛﾟ)っ\r\n"
-				+ "　　　(っﾉ\r\n"
-				+ "　　　 `Ｊ"
-		);
+		System.out.println("　　　가\r\n" + "　　 아\r\n" + "　　　지\r\n" + "　　　　마\r\n" + "　　　　 ㅇ\r\n" + "　　　　ㅏ\r\n"
+				+ "　 ヽ＼　 /／\r\n" + "　　　 ,､,､　ﾟ｡\r\n" + "　 ﾟ　(ﾟﾛﾟ)っ\r\n" + "　　　(っﾉ\r\n" + "　　　 `Ｊ");
 	}
 
 	private static void goodbyeMessage() {
 		System.out.println("✿°•∘ɷ∘•°✿ ... ✿°•∘ɷ∘•°✿ 또 봐요 ✿°•∘ɷ∘•°✿ ... ✿°•∘ɷ∘•°✿");
-	    System.out.println(". ˚\r\n"
-	    		+ "*  * 　　　 ⊹  ˚  .    　　.\r\n"
-	    		+ "⊹  ·  ✧ 　　　*    *\r\n"
-	    		+ ". 　　  ⊹  ✦  　 ✵  　　　　　*\r\n"
-	    		+ "* .　  ·"
-		);
+		System.out.println(". ˚\r\n" + "*  * 　　　 ⊹  ˚  .    　　.\r\n" + "⊹  ·  ✧ 　　　*    *\r\n"
+				+ ". 　　  ⊹  ✦  　 ✵  　　　　　*\r\n" + "* .　  ·");
 	}
 }
